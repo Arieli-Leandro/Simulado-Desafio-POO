b5abc79ccf779720c6e3398b60ed4ac3c178d15c
Arieli Leandro Gutierres <arieli.leandrogutierres2005@gmail.com>
2026-06-02 15:09:42 -0300

    Refactor Usuario class and add loan management classes
    Refactor Usuario class to be abstract and add prazoHoras method to subclasses. Implement Emprestimo and Chave classes for managing loans and key status.

--- Versao Python/heimdall.py
+++ Versao Python/heimdall.py
@@ -1,6 +1,10 @@
+import datetime
+from datetime import timedelta
+from abc import ABC, abstractmethod
+from enum import Enum
 
 #Classe Usuário finalizada
-class Usuario:
+class Usuario(ABC):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
         self._id = id_u
         self._nome = nome_u
@@ -58,6 +62,11 @@ class Usuario:
     def podeRetirar(self):
         return False
     
+    @abstractmethod
+    def prazoHoras(self):
+        return 0
+        
+    
         
 #Classe Professor -> Herda de Usuário
 class Professor(Usuario):
@@ -66,6 +75,9 @@ class Professor(Usuario):
 
     def podeRetirar(self):
         return True
+    
+    def prazoHoras(self):
+        return 24
 
 #Classe Tecnico -> Herda de Usuário
 class Tecnico(Usuario):
@@ -75,7 +87,11 @@ class Tecnico(Usuario):
     def podeRetirar(self):
         return True
     
+    def prazoHoras(self):
+        return 8
+    
 #Classe Administrador -> Herda de Usuário mas ela tem métodos adicionais como (Cadastrar Chave e CadastrarUser)
+# O administrador tbm gera relatório
 class Administrador(Usuario):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
         super().__init__(id_u, nome_u, matricula_u, email_u, ativo_u)
@@ -89,9 +105,60 @@ class Administrador(Usuario):
     def cadastrarUsuario(self):
         pass
 
+    def gerarRelatorio(self):
+        pass
+
+#Classe Emprestimo
+class Emprestimo:
+    def __init__(self, id_u, usuario, chave_u, justificativa):
+        self._id = id_u
+        self._usuario = usuario
+        self._chave = chave_u
+
+        self._dataRetirada = datetime.time()
+        horas_usuario = usuario.prazoHoras()
+        self._dataPrevista =  self._dataPrevista + timedelta(hours=horas_usuario)
+        self._dataDevolucao
+
+        self._justificativa = justificativa
 
 
 
+    def encerrarEmprestimo(self):
+        pass
+
+    def verificaAtraso(self):
+        pass
+
+class StatusChave(Enum):
+    DISPONIVEL = 1
+    EMPRESTADA = 2
+    EM_ATRASO = 3
+
+
+class Chave:
+    def __init__(self, id_u, cod_u, ambiente):
+        self._id = id_u
+        self._codigo = cod_u
+        self._ambiente = ambiente
+        self._status = StatusChave.DISPONIVEL
+
+    def verificaChaveDisponivel(self, chave):
+        retorno = False
+
+        if(chave._status == StatusChave.DISPONIVEL):
+            retorno = True
+
+        return retorno
+
+class Ambiente:
+    def __init__(self, id_u, nome, bloco_u, desc):
+        self._id = id_u
+        self._nome = nome
+        self._bloco = bloco_u
+        self._descricao = desc
+
+    #Não tem métodos, a não ser q eu queira fazer um exibe_Ambiente
 
 if __name__ == "__main__":
     pass
