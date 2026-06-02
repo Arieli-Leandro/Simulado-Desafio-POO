242a7cec05a7145c16ff0748b5c9cea9403ccb9e
Arieli Leandro Gutierres <arieli.leandrogutierres2005@gmail.com>
2026-06-02 15:27:49 -0300

    Refactor Usuario class and add GerenciadorDeSistemaHeimdall

--- Versao Python/heimdall.py
+++ Versao Python/heimdall.py
@@ -100,12 +100,15 @@ class Administrador(Usuario):
         return True
     
     def cadastrarChave(self):
+        #Chama o sistema Heimdall
         pass
 
     def cadastrarUsuario(self):
+        #Chama o sistema Heimdall
         pass
 
     def gerarRelatorio(self):
+        #Pega o vetor de registros e cria um arquivo com ele
         pass
 
 #Classe Emprestimo
@@ -122,13 +125,57 @@ class Emprestimo:
 
         self._justificativa = justificativa
 
+    #Métodos getters
+    def getId(self):
+        return self._id
+    
+    def getUsuario(self):
+        return self._usuario
+    
+    def getChave(self):
+        return self._chave
+    
+    def getDataRetirada(self):
+        return self._dataRetirada
+    
+    def getDataPrevista(self):
+        return self._dataPrevista
+    
+    def getDataDevolucao(self):
+        return self._dataDevolucao
+    
+    def getJustificativa(self):
+        return self._justificativa
 
+    #Métodos setters
+    def setId(self, id_u):
+        self.id = id_u
+        return
+    
+    def setUsuario(self, usuario):
+        self._usuario = usuario
+        return
+    
+    def setChave(self, chave):
+        self._chave = chave
+        return
+    
+    def setJustificativa(self, justificativa):
+        self._justificativa = justificativa
+        return
 
+    #Métodos da classe
     def encerrarEmprestimo(self):
-        pass
+        self._dataDevolucao = datetime.time()
 
     def verificaAtraso(self):
-        pass
+        retorno = False
+
+        if(self._dataDevolucao > self._dataPrevista):
+            print("A chave foi devolvida com atraso!")
+            retorno = True
+
+        return retorno
 
 class StatusChave(Enum):
     DISPONIVEL = 1
@@ -143,6 +190,41 @@ class Chave:
         self._ambiente = ambiente
         self._status = StatusChave.DISPONIVEL
 
+    #Métodos getters
+    def getId(self):
+        return self._id
+    
+    def getCodigo(self):
+        return self._codigo
+    
+    def getAmbiente(self):
+        return self._ambiente
+    
+    def getStatus(self):
+        return self._status
+
+    #Métodos setters
+    def setId(self, id_u):
+        self._id = id_u
+        return
+    
+    def setCodigo(self, codigo):
+        self._codigo = codigo
+        return
+    
+    def setAmbiente(self, ambiente):
+        self._ambiente = ambiente
+        return
+    
+    def setStatus(self, status):
+        #A ideia é lançar um erro aqui se não foi do tipo StatusChave
+        #Depois fazer uma classe de erro para chave inválida
+        if isinstance(status, StatusChave):
+            raise TypeError("Tipo de chave inválida!")
+        
+        self._status = status
+
+    #Métodos da Classe
     def verificaChaveDisponivel(self, chave):
         retorno = False
 
@@ -158,7 +240,41 @@ class Ambiente:
         self._bloco = bloco_u
         self._descricao = desc
 
+    #Métodos getters
+    def getId(self):
+        return self._id
+    
+    def getNome(self):
+        return self._nome
+    
+    def getBloco(self):
+        return self._bloco
+    
+    def getDescricao(self):
+        return self._descricao
+
+    #Métodos setters
+    def setId(self, id_u):
+        self._id = id_u
+        return
+    
+    def setNome(self, nome):
+        self._nome = nome
+        return
+    
+    def setBloco(self, bloco):
+        self._bloco = bloco
+        return
+    
+    def setDescricao(self, desc):
+        self._descricao = desc
+        return
+    
     #Não tem métodos, a não ser q eu queira fazer um exibe_Ambiente
+
+class GerenciadorDeSistemaHeimdall:
+    def __init__(self):
+        pass
 
 if __name__ == "__main__":
     pass
