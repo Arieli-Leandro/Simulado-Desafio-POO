3d0af2c9d9e374be25d85fd0c917a3cc2f14486e
Arieli Leandro Gutierres <arieli.leandrogutierres2005@gmail.com>
2026-06-02 22:40:08 -0300

    Add exception classes and refactor user methods
    Graças a deus falta só +3 métodos de Adm e a main

--- Versao Python/heimdall.py
+++ Versao Python/heimdall.py
@@ -3,9 +3,70 @@ from datetime import timedelta
 from abc import ABC, abstractmethod
 from enum import Enum
 
+# =- Excessções -=
+
+class ExcessaoUsuarioInativo(Exception):
+
+    def __str__(self):
+        return f"Erro: O Usuário está inativo!"
+
+class ExcesssaoChaveIndisponivel(Exception):
+
+    def __str__(self):
+        return f"Erro: A chave está indisponível para o emprestimo!"
+
+class ExcessaoUsuarioNaoPodeRetirarChave(Exception):
+
+    def __str__(self):
+        return f"Erro: Usuário não tem permissão para retirar a chave!"
+
+class ExcessaoEmprestimoInvalido(Exception):
+
+    def __str__(self):
+        return f"Erro: Emprestimo inválido!"
+    
+class ExcessaoCadastroUsuarioExistente(Exception):
+
+    def __str__(self):
+        return f"O Usuário já está cadastrado!"
+    
+class ExcessaoUsuarioNaoExiste(Exception):
+
+    def __str__(self):
+        return f"O Usuário não existe!"
+
+class ExcessaoCadastroChaveExistente(Exception):
+
+    def __str__(self):
+        return f"A chave já foi cadastrada anteriormente!"
+
+class ExcessaoChaveNaoExiste(Exception):
+
+    def __str__(self):
+        return f"A chave não existe!"
+    
+class ExcessaoCadastroAmbienteExiste(Exception):
+
+    def __str__(self):
+        return f"O ambiente já foi cadastrado anteriormente!"
+    
+class ExcessaoAmbienteNaoExiste(Exception):
+
+    def __str__(self):
+        return f"O ambiente não existe!"
+    
+class ExcessaoDevolucaoNaoEncontrada(Exception):
+
+    def __str__(self):
+        return f"Registro de Emprestimo não encontrado, não foi possível realizar devolução!"
+
+
+# =-- Classes --=
+
 #Classe Usuário finalizada
 class Usuario(ABC):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
+
         self._id = id_u
         self._nome = nome_u
         self._matricula = matricula_u
@@ -14,56 +75,68 @@ class Usuario(ABC):
 
     #Métodos getters
     def getId(self):
+
         return self._id
     
     def getNome(self):
+
         return self._nome
     
     def getMatricula(self):
+
         return self._matricula
     
     def getEmail(self):
+
         return self._email
     
     def getAtivo(self):
+
         return self._ativo
 
     #Métodos setters
     def setId(self, id_u):
+
         self._id = id_u
         return 
 
     def setNome(self, nome):
+
         self._nome = nome
         return
     
     def setMatricula(self, matricula):
+
         self._matricula = matricula
         return
     
     def setEmail(self, email):
+
         self._email = email
         return
     
     def setAtivo(self, ativo):
+
         self._ativo = ativo
         return
 
     #Métodos específicos da classe
     def autenticar(self):
+
         retorno = False
 
         if(self._ativo == True):
             retorno = True
         
-
         return retorno
     
     def podeRetirar(self):
+
         return False
     
     @abstractmethod
     def prazoHoras(self):
+
         return 0
         
     
@@ -71,29 +144,36 @@ class Usuario(ABC):
 #Classe Professor -> Herda de Usuário
 class Professor(Usuario):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
+
         super().__init__(id_u, nome_u, matricula_u, email_u, ativo_u)
 
     def podeRetirar(self):
+
         return True
     
     def prazoHoras(self):
+
         return 24
 
 #Classe Tecnico -> Herda de Usuário
 class Tecnico(Usuario):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
+
         super().__init__(id_u, nome_u, matricula_u, email_u, ativo_u)
 
     def podeRetirar(self):
+
         return True
     
     def prazoHoras(self):
+
         return 8
     
 #Classe Administrador -> Herda de Usuário mas ela tem métodos adicionais como (Cadastrar Chave e CadastrarUser)
 # O administrador tbm gera relatório
 class Administrador(Usuario):
     def __init__(self, id_u, nome_u, matricula_u, email_u, ativo_u):
+
         super().__init__(id_u, nome_u, matricula_u, email_u, ativo_u)
 
     def podeRetirar(self):
@@ -114,6 +194,7 @@ class Administrador(Usuario):
 #Classe Emprestimo
 class Emprestimo:
     def __init__(self, id_u, usuario, chave_u, justificativa):
+
         self._id = id_u
         self._usuario = usuario
         self._chave = chave_u
@@ -127,48 +208,61 @@ class Emprestimo:
 
     #Métodos getters
     def getId(self):
+
         return self._id
     
     def getUsuario(self):
+
         return self._usuario
     
     def getChave(self):
+
         return self._chave
     
     def getDataRetirada(self):
+
         return self._dataRetirada
     
     def getDataPrevista(self):
+
         return self._dataPrevista
     
     def getDataDevolucao(self):
+
         return self._dataDevolucao
     
     def getJustificativa(self):
+
         return self._justificativa
 
     #Métodos setters
     def setId(self, id_u):
+
         self.id = id_u
         return
     
     def setUsuario(self, usuario):
+
         self._usuario = usuario
         return
     
     def setChave(self, chave):
+
         self._chave = chave
         return
     
     def setJustificativa(self, justificativa):
+
         self._justificativa = justificativa
         return
 
     #Métodos da classe
     def encerrarEmprestimo(self):
+
         self._dataDevolucao = datetime.time()
 
     def verificaAtraso(self):
+
         retorno = False
 
         if(self._dataDevolucao > self._dataPrevista):
@@ -178,6 +272,7 @@ class Emprestimo:
         return retorno
 
 class StatusChave(Enum):
+
     DISPONIVEL = 1
     EMPRESTADA = 2
     EM_ATRASO = 3
@@ -185,6 +280,7 @@ class StatusChave(Enum):
 
 class Chave:
     def __init__(self, id_u, cod_u, ambiente):
+
         self._id = id_u
         self._codigo = cod_u
         self._ambiente = ambiente
@@ -192,27 +288,34 @@ class Chave:
 
     #Métodos getters
     def getId(self):
+
         return self._id
     
     def getCodigo(self):
+
         return self._codigo
     
     def getAmbiente(self):
+
         return self._ambiente
     
     def getStatus(self):
+
         return self._status
 
     #Métodos setters
     def setId(self, id_u):
+
         self._id = id_u
         return
     
     def setCodigo(self, codigo):
+
         self._codigo = codigo
         return
     
     def setAmbiente(self, ambiente):
+
         self._ambiente = ambiente
         return
     
@@ -226,6 +329,7 @@ class Chave:
 
     #Métodos da Classe
     def verificaChaveDisponivel(self, chave):
+
         retorno = False
 
         if(chave._status == StatusChave.DISPONIVEL):
@@ -234,7 +338,9 @@ class Chave:
         return retorno
 
 class Ambiente:
+
     def __init__(self, id_u, nome, bloco_u, desc):
+
         self._id = id_u
         self._nome = nome
         self._bloco = bloco_u
@@ -242,44 +348,56 @@ class Ambiente:
 
     #Métodos getters
     def getId(self):
+
         return self._id
     
     def getNome(self):
+
         return self._nome
     
     def getBloco(self):
+
         return self._bloco
     
     def getDescricao(self):
+
         return self._descricao
 
     #Métodos setters
     def setId(self, id_u):
+
         self._id = id_u
         return
     
     def setNome(self, nome):
+
         self._nome = nome
         return
     
     def setBloco(self, bloco):
+
         self._bloco = bloco
         return
     
     def setDescricao(self, desc):
+
         self._descricao = desc
         return
     
     #Não tem métodos, a não ser q eu queira fazer um exibe_Ambiente
 
+
 class GerenciadorDeSistemaHeimdall:
+
     def __init__(self):
+
         self._lista_usuarios = []
         self._lista_chaves = []
         self._lista_emprestimos = []
         self._lista_ambientes = []
 
     def _verificaSeUsuarioExiste(self, usuario):
+
         retorno = False
 
         for i in self._lista_usuarios:
@@ -312,13 +430,15 @@ class GerenciadorDeSistemaHeimdall:
         return retorno
 
     def cadastrarUsuario(self, usuario):
+
         if(self._verificaSeUsuarioExiste(usuario) == False):
             self._lista_usuarios.append(usuario)
         else:
-            print("O usuário já existe!")
+            raise ExcessaoCadastroUsuarioExistente()
         return
 
     def deletarUsuario(self, usuario):
+
         if(self._verificaSeUsuarioExiste(usuario) == True):
             #encontra o indide, tira da lista e depois deleta o obj
             for indice, x in enumerate(self._lista_usuarios):
@@ -327,17 +447,19 @@ class GerenciadorDeSistemaHeimdall:
                     del usuario
                     break
         else:
-            print("Para deletar um usuário o cadastro dele tem que existir!")
+           raise ExcessaoUsuarioNaoExiste()
 
     def cadastrarChave(self, chave):
+
         if(self._verificaSeChaveExiste(chave) == False):
             self._lista_chaves.append(chave)
         else:
-            print("A chave já está cadastrada!")
+            raise ExcessaoCadastroChaveExistente()
 
         return
 
     def deletarChave(self, chave):
+
         if(self._verificaSeChaveExiste(chave) == True):
 
             for indice, x in enumerate(self._lista_chaves):
@@ -346,21 +468,23 @@ class GerenciadorDeSistemaHeimdall:
                     del chave
                     break
         else:
-            print("Para deletar uma chave, o cadastro dela tem que existir!")
+            raise ExcessaoChaveNaoExiste()
 
         return
 
     def cadastrarAmbiente(self, ambiente):
+
         if(self._verificaSeAmbienteExiste(ambiente) == False):
             self._lista_ambientes.append(ambiente)
         else:
-            print("O ambiente já existe!")
+            raise ExcessaoCadastroAmbienteExiste()
 
         return
         
 
     #Transformar o else em Exception
     def deletarAmbiente(self, ambiente):
+
         if(self._verificaSeAmbienteExiste(ambiente) == True):
 
             for indice, x in enumerate(self._lista_ambientes):
@@ -369,7 +493,7 @@ class GerenciadorDeSistemaHeimdall:
                     del ambiente
                     break
         else:
-            print("Para deletar um ambiente, ele precisa estar cadastrado!")
+            raise ExcessaoAmbienteNaoExiste()
 
         return
 
@@ -381,7 +505,7 @@ class GerenciadorDeSistemaHeimdall:
         if(usuario.podeRetirar() == True):
             retorno = True
         else:
-            print(f"O Usuário: {usuario.getNome()} não pode retirar uma chave!")
+            raise ExcessaoUsuarioNaoPodeRetirarChave()
 
         return retorno
 
@@ -393,7 +517,7 @@ class GerenciadorDeSistemaHeimdall:
         if(chave.getStatus() == StatusChave.DISPONIVEL):
             retorno = True
         else:
-            print("Chave não está disponível!")
+            raise ExcesssaoChaveIndisponivel()
 
         return retorno
     
@@ -405,7 +529,7 @@ class GerenciadorDeSistemaHeimdall:
         if(usuario.getAtivo() == True):
             retorno = True
         else:
-            print("O Usuário não está ativo!")
+            raise ExcessaoUsuarioInativo()
 
         return retorno
 
@@ -432,12 +556,15 @@ class GerenciadorDeSistemaHeimdall:
             #Acessando e mudando o estado da chave
             chave.setStatus(StatusChave.EMPRESTADA)
         else:
-            pass
+            raise ExcessaoEmprestimoInvalido()
 
         return
         
 
     def registraDevolucao(self, chave):
+
+        #Vai ser capaz de informar se existe a chave no registro para "devolver"
+        verificador = -1
         
         #-> Muda o estado da chave para disponivel novamente
         for emprestimo in self._lista_emprestimos:
@@ -446,9 +573,15 @@ class GerenciadorDeSistemaHeimdall:
                 #Encerro o emprestimo dessa chave X
                 emprestimo.encerrarEmprestimo()
 
+                #Troca o estado 
+                verificador = 1
+
                 #Mudo o estado da chave X para disponivel
                 chave.setStatus(StatusChave.DISPONIVEL)
                 break
+
+        if(verificador == -1):
+            raise ExcessaoDevolucaoNaoEncontrada()
 
         return
 
